32f0efe834e86d10cd415153b89684f12a07a2fe
bixto123 <bixto@hotmail.com>
2022-12-19 18:36:07 +0100

    Modification App.js et Square.css

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import {SplitPane} from "./components/SplitPane";
 import "./css/SplitPane.css";
 import {SignUpDialog} from "./components/SignUpDialog";
 import "./css/components/SignUpDialog.css";
+import {ExampleHook} from "./components/ExampleHook";
 */
 
 /*
@@ -64,9 +65,9 @@ const posts = [
   <Calculator />
   <SplitPane left={<Contacts />} right={<Chat />} />
   <SignUpDialog />
+  <ExampleHook />
 */
 
-import {ExampleHook} from "./components/ExampleHook";
 import {useEffect, useRef} from 'react';
 import "./css/Square.css";
 
@@ -75,10 +76,13 @@ function Square(props) {
 }
 
 function Row(props) {
-  let number = 20;
+  let width = 20;
+  let square_key = 0;
   const elements = [];
-  while(number--)
-    elements.push(<Square color={props.color} />);
+
+  while(width--)
+    elements.push(<Square key={square_key++} color={props.color} />);
+
   return (
     <div className="Row">
       {elements}
@@ -88,32 +92,41 @@ function Row(props) {
 
 function Board() {
   let position = 0;
+  let row_key = 0;
+
   function handleKeyDown(event) {
     const board = document.querySelector(".Board");
+
     //console.log('User pressed: ', event.key);
+
     if(event.key === "ArrowLeft") {
-      if(position > -160)
-        position--;
-      board.style.left = position.toString() + "px";
-    }
-    if(event.key === "ArrowRight") {
       if(position < 0)
         position++;
       board.style.left = position.toString() + "px";
     }
-  };
+
+    if(event.key === "ArrowRight") {
+      if(position > -160)
+        position--;
+      board.style.left = position.toString() + "px";
+    }
+  }
+
   const ref = useRef(null);
+
   useEffect(() => {
     ref.current.focus();
   }, []);
-  let number = 9;
+
+  let height = 10;
   const elements = [];
-  while(number--)
-    elements.push(<Row color="skyblue" />);  
+
+  while(height--)
+    elements.push(<Row key={row_key++} color="skyblue" />);
+  
   return (
     <div className="Board" ref={ref} tabIndex={-1} onKeyDown={handleKeyDown}>
       {elements}
-      <Row color="brown" />
     </div>
   );
 }
@@ -124,7 +137,6 @@ function App() {
       <div className="Container">
         <Board />
       </div>
-      <ExampleHook />
     </div>
   );
 }
